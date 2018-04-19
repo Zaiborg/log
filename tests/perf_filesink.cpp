6fcaaf266a41a86574bb32ac5c9370bdee2b0449
Zaiborg <zaiborg@icloud.com>
2018-04-19 22:57:21 +0200

    Add loop to ensure logging wchar_t and char does not cause side effects

--- tests/perf_filesink.cpp
+++ tests/perf_filesink.cpp
@@ -24,21 +24,28 @@ int main(int argc, char** argv) {
         zaiborg::Logger console(new zaiborg::ConsoleSink);
 
         tmr t;
-        t.start();
-        for (int i = 0; i < 100000; ++i)
-                file_log.write("zaiborg::Logger printing message nr. %d -> Hello World!", i);
-        auto w1 = std::chrono::duration_cast<std::chrono::microseconds>(
-                t.ellapsed());
 
-        console.write("logging 100000 ascii entries took\t%luus", 
-                std::chrono::duration_cast<std::chrono::microseconds>(w1).count());
+        for (int n = 0; n < 10; ++n) {
+                t.start();
+                for (int i = 0; i < 100000; ++i)
+                        file_log.write("zaiborg::Logger printing message nr. %d -> Hello World!", i);
+                auto w1 = std::chrono::duration_cast<std::chrono::microseconds>(
+                        t.ellapsed());
 
-        t.start();
-        for (int i = 0; i < 100000; ++i)
-                file_log.write(L"zaiborg::Logger printing message nr. %d -> 「こんにちは世界」", i);
-        auto w2 = std::chrono::duration_cast<std::chrono::microseconds>(
-                t.ellapsed());
+                console.write("logging 100000 ascii entries took\t%luus", 
+                        std::chrono::duration_cast<std::chrono::microseconds>(w1).count());
 
-        console.write("logging 100000 unicode entries took\t%luus", 
-                std::chrono::duration_cast<std::chrono::microseconds>(w2).count());
+                t.start();
+                for (int i = 0; i < 100000; ++i)
+                        file_log.write(L"zaiborg::Logger printing message nr. %d -> 「こんにちは世界」", i);
+                auto w2 = std::chrono::duration_cast<std::chrono::microseconds>(
+                        t.ellapsed());
+
+                console.write("logging 100000 unicode entries took\t%luus", 
+                        std::chrono::duration_cast<std::chrono::microseconds>(w2).count());
+
+        }
+
+
+
 }
