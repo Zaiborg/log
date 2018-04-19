3bbbf659e883fc4e8bbfbdf9fda126e6e31be6f8
Zaiborg <zaiborg@icloud.com>
2018-04-19 21:21:55 +0200

    Fix define snprintf function to avoid compile errors under linux Fix include for std::runtime_error

--- include/platform.hpp
+++ include/platform.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
-#include <exception>
+#include <stdexcept>
 
 #ifdef _WIN32
 
 #include <io.h>
 #include <fcntl.h>
 #include <iostream>
+#include <stdarg.h>
 
 namespace zaiborg
 {
@@ -16,6 +17,20 @@ namespace zaiborg
 
         inline static void change_console_to_unicode() {
                 _setmode(_fileno(stdout), _O_WTEXT);
+        }
+
+        inline static int snprintf(char* b, size_t s, const char* f, ...) {
+                va_list args;
+                va_start(args, f);
+                return vsnprintf(b, s, f, args);
+                va_end(args);
+        }
+        
+        inline static int snprintf(wchar_t* b, size_t s, const wchar_t* f, ...) {
+                va_list args;
+                va_start(args, f);
+                return _vsnwprintf_s(b, s, s, f, args);
+                va_end(args);
         }
 }
 
@@ -30,5 +45,14 @@ namespace zaiborg
         inline static void change_console_to_unicode() {
                 throw std::runtime_error("TODO: not implemented yet");
         }
+
+        inline static int snprintf(char* b, size_t s, char* f, ...) {
+                throw std::runtime_error("TODO: not implemented yet");
+        }
+
+        inline static int snprintf(wchar_t* b, size_t s, wchar_t* f, ...) {
+                throw std::runtime_error("TODO: not implemented yet");
+        }
+
 }
 #endif
