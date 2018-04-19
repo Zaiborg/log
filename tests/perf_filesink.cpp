43a62b0b76b4e3caa727911165305662c78b742c
Zaiborg <zaiborg@icloud.com>
2018-04-19 22:28:45 +0200

    Add performance test for FileSink

---
+++ tests/perf_filesink.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <chrono>
+
+#include "../../include/logger.hpp"
+
+struct tmr
+{
+        tmr() {}
+        
+        void start(){
+                _start = std::chrono::high_resolution_clock::now();
+        }
+
+        std::chrono::nanoseconds ellapsed()const {
+                auto end = std::chrono::high_resolution_clock::now();
+                return end - _start;
+        }
+
+        std::chrono::time_point<std::chrono::high_resolution_clock> _start;
+};
+
+int main(int argc, char** argv) {
+        zaiborg::Logger file_log(new zaiborg::FileSink("test.log"));
+        zaiborg::Logger console(new zaiborg::ConsoleSink);
+
+        tmr t;
+        t.start();
+        for (int i = 0; i < 100000; ++i)
+                file_log.write("zaiborg::Logger printing message nr. %d -> Hello World!", i);
+        auto w1 = std::chrono::duration_cast<std::chrono::microseconds>(
+                t.ellapsed());
+
+        console.write("logging 100000 ascii entries took\t%luus", 
+                std::chrono::duration_cast<std::chrono::microseconds>(w1).count());
+
+        t.start();
+        for (int i = 0; i < 100000; ++i)
+                file_log.write(L"zaiborg::Logger printing message nr. %d -> 「こんにちは世界」", i);
+        auto w2 = std::chrono::duration_cast<std::chrono::microseconds>(
+                t.ellapsed());
+
+        console.write("logging 100000 unicode entries took\t%luus", 
+                std::chrono::duration_cast<std::chrono::microseconds>(w2).count());
+}
