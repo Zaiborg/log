a67540e617a685d8f583a7f38cddf1721365d7e7
Zaiborg <zaiborg@icloud.com>
2018-04-19 22:24:49 +0200

    Add basic file sink

--- include/sink.hpp
+++ include/sink.hpp
@@ -2,6 +2,10 @@
 
 #include "platform.hpp"
 
+#include <string>
+#include <fstream>
+#include <codecvt>
+
 namespace zaiborg
 {
         class Sink
@@ -22,5 +26,25 @@ namespace zaiborg
                         zaiborg::change_console_to_unicode();
                         std::wcout << line << std::endl;
                 }
+        };
+
+        class FileSink : public Sink
+        {
+        public:
+                FileSink(const std::string& filename) 
+                {
+                        fs.open(filename.c_str(), std::wfstream::out | std::wfstream::app);
+                        fs.imbue(std::locale(fs.getloc(), new std::codecvt_utf8_utf16<wchar_t>()));
+                }
+
+                virtual void write(const std::wstring& line)override {
+                        fs << line << std::endl;
+                }
+                virtual void write(const std::string& line) override {
+                        write(std::wstring(line.begin(), line.end()));
+                }
+
+                std::wfstream fs;
+
         };
 }
