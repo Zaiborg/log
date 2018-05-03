cdbd6dd56d2a39d4e656daa9cdaf8b2ad80b7d8c
Zaiborg <zaiborg@icloud.com>
2018-05-03 20:31:20 +0200

    Adapt performance test to work with previous changes

--- tests/perf_filesink.cpp
+++ tests/perf_filesink.cpp
@@ -19,16 +19,20 @@ struct tmr
         std::chrono::time_point<std::chrono::high_resolution_clock> _start;
 };
 
+
 int main(int argc, char** argv) {
-        zaiborg::Logger file_log(new zaiborg::FileSink("test.log"));
-        zaiborg::Logger console(new zaiborg::ConsoleSink);
+
+        zaiborg::Logger<char> afile_log(new zaiborg::FileSink("test_ascii.log"));
+        zaiborg::Logger<wchar_t> wfile_log(new zaiborg::FileSink("test_unicode.log"));
+        zaiborg::Logger<char> console(new zaiborg::ConsoleSink);
+
 
         tmr t;
 
         for (int n = 0; n < 10; ++n) {
                 t.start();
                 for (int i = 0; i < 100000; ++i)
-                        file_log.write("zaiborg::Logger printing message nr. %d -> Hello World!", i);
+                        afile_log.write("zaiborg::Logger printing message nr. %d -> Hello World! and some garbage: %s", i, "aaaaaaaaaaaaaaaaaaeeeeeeeeeeeeeeeeeeeeeeeiiiiiiiiiiiiiiiiiiiiiiooooooooooooooooooooouuuuuuuuuuuuuuuuuu");
                 auto w1 = std::chrono::duration_cast<std::chrono::microseconds>(
                         t.ellapsed());
 
@@ -37,7 +41,7 @@ int main(int argc, char** argv) {
 
                 t.start();
                 for (int i = 0; i < 100000; ++i)
-                        file_log.write(L"zaiborg::Logger printing message nr. %d -> 「こんにちは世界」", i);
+                        wfile_log.write(L"zaiborg::Logger printing message nr. %d -> 「こんにちは世界」 and some garbage: %ls", i, L"aaaaaaaaaaaaaaaaaaeeeeeeeeeeeeeeeeeeeeeeeiiiiiiiiiiiiiiiiiiiiiiooooooooooooooooooooouuuuuuuuuuuuuuuuuu");
                 auto w2 = std::chrono::duration_cast<std::chrono::microseconds>(
                         t.ellapsed());
 
