f0a89db30de26e816c9640a4c108a2826522720b
Zaiborg <zaiborg@icloud.com>
2018-05-03 20:13:33 +0200

    FIX prevent mixing ascii and unicode messages that caused undefined behaviour

--- include/logger.hpp
+++ include/logger.hpp
@@ -10,9 +10,29 @@
 
 namespace zaiborg
 {
+        template <typename char_t>
         class Logger
         {
         public:
+                template <typename T>
+                const T& check_string(const T& arg) {
+                        return arg;
+                }
+
+                template <typename T>
+                const std::basic_string<T>& check_string(const std::basic_string<T>& arg) {
+                        static_assert(std::is_same<T, char_t>::value,
+                                "invalid string in arguments");
+                        return arg;
+                }
+                template <typename T>
+                const T* check_string(const T* arg) {
+                        static_assert(std::is_same<T, char_t>::value,
+                                "invalid string in arguments");
+                        return arg;
+                }
+
+
                 Logger() {}
 
                 explicit Logger(std::unique_ptr<Sink>&& s) {
@@ -21,28 +41,26 @@ namespace zaiborg
                 explicit Logger(Sink* s) : Logger(std::unique_ptr<Sink>(s)) {
                 }
 
-
                 template <typename ...Args>
-                void write(const std::string& format, Args ...args) {
-                        write_impl(format, args...);
-                }
-                void write(const std::string& format) {
-                        write_impl(std::string("%s"), format);
+                void write(const std::basic_string<char_t>& format, Args ...args) {
+                        write_impl(format, check_string(args)...);
                 }
 
-                template <typename ...Args>
-                void write(const std::wstring& format, Args ...args) {
-                        write_impl(format, args...);
+                template <typename C = char_t>
+                typename std::enable_if<std::is_same<char, C>::value>::type
+                write(const std::basic_string<char>& format) {
+                        write_impl(std::basic_string<char>("%s"), format);
                 }
 
-                void write(const std::wstring& format) {
-                        write_impl(std::wstring(L"%ls"), format);
+                template <typename C = char_t>
+                typename std::enable_if<std::is_same<wchar_t, C>::value>::type
+                write(const std::basic_string<wchar_t>& format) {
+                        write_impl(std::basic_string<wchar_t>(L"%ls"), format);
                 }
 
 
         private:
                 std::vector<std::unique_ptr<Sink>> sinks;
-
 
                 template<typename string_t, typename ...Args>
                 void write_impl(const string_t& format, Args ...args) {
